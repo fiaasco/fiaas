a1773a5e0ab9e1d27d71006079e39a6daf010fb3
Luc Stroobant <luc@warp.be>
2017-11-18 17:20:12 +0100

    letsencrypt shared webroot

--- playbooks/roles/createresources/templates/request-vhost.sh
+++ playbooks/roles/createresources/templates/request-vhost.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # acme request cert template for vhost {{ item.url }}
-# This supposes you have a line in Drupal .htaccess that allows this type of requests
-# https://www.drupal.org/node/2408321
-# -  RewriteRule "(^|/)\." - [F]
-# +  RewriteRule "(^|/)\.(?!well-known)" - [F]
 
-
-/root/.acme.sh/acme.sh --issue --webroot {{ item.basedir }}/web -d {{ item.url }} {% if item.alias is defined %}{% for alias in item.alias %} -d {{ alias }} {% endfor %}{% endif %}
+/root/.acme.sh/acme.sh --issue --webroot /var/www/acme -d {{ item.url }} {% if item.alias is defined %}{% for alias in item.alias %} -d {{ alias }} {% endfor %}{% endif %}
 
 STATUS=$?
 
